208673923693229f521008f32e97df32e4e3c46d
Benjamin Herson & Jackie Fletcher <student@epicodus.com>
2015-02-02 14:17:40 -0800

    add game_id migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202191639) do
+ActiveRecord::Schema.define(version: 20150202220630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150202191639) do
     t.integer  "score"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "game_id"
   end
 
 end
