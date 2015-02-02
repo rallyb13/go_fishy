30cf4a83ea5f19bac43cb128605ec281caec2309
Benjamin Herson & Jackie Fletcher <student@epicodus.com>
2015-02-02 15:16:54 -0800

    add get_card method

--- lib/player.rb
+++ lib/player.rb
@@ -3,4 +3,15 @@ class Player < ActiveRecord::Base
   has_and_belongs_to_many :cards
   belongs_to :game
 
+  def get_card (number)
+    number.times do
+      card = Card.not_dealt.take
+      card.update(dealt: true)
+      self.cards.push(card)
+    end
+  end
+
+  # def ask_for (opponent, my_card)
+  #   opponent.cards.fish == my_card
+  # end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,11 +4,33 @@ describe(Player) do
   it {should have_and_belong_to_many :cards}
   it {should belong_to :game}
 
+
+  describe('#get_card') do
+    it('will deal a player a card') do
+      card1 = Card.create(fish: "beardfish", dealt: false)
+      player1 = Player.create()
+      player1.get_card(1)
+      expect(player1.cards.first.dealt).to eq(true)
+    end
+  end
+
+  # describe('#ask_for') do
+  #   it('will ask an opponent if they have a certain card') do
+  #     card1 = Card.create(fish: "beardfish")
+  #     card2 = Card.create(fish: "beardfish")
+  #     player1 = Player.create()
+  #     player2 = Player.create()
+  #     player1.cards.push(card1)
+  #     player2.cards.push(card2)
+  #     expect(player1.ask_for(player2, card1.fish)).to eq(true)
+  #   end
+  # end
+
   describe "check_doubles" do
     player1 = Player.new()
     player1.get_cards()
     expect(player1.check_doubles()).to eq(true)
   end
-
+  
 
 end
