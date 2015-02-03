7645dca295d9833b9c29f201dea74c47c9abb16c
Benjamin Herson & Jackie Fletcher <student@epicodus.com>
2015-02-02 16:16:08 -0800

    add ask for method in player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -14,23 +14,39 @@ describe(Player) do
     end
   end
 
-  # describe('#ask_for') do
-  #   it('will ask an opponent if they have a certain card') do
-  #     card1 = Card.create(fish: "beardfish")
-  #     card2 = Card.create(fish: "beardfish")
-  #     player1 = Player.create()
-  #     player2 = Player.create()
-  #     player1.cards.push(card1)
-  #     player2.cards.push(card2)
-  #     expect(player1.ask_for(player2, card1.fish)).to eq(true)
-  #   end
-  # end
-
-  describe "check_doubles" do
-    player1 = Player.new()
-    player1.get_cards()
-    expect(player1.check_doubles()).to eq(true)
+  describe('#ask_for') do
+    it('will ask an opponent if they have a certain card') do
+      card1 = Card.create(fish: "beardfish")
+      card2 = Card.create(fish: "beardfish")
+      card3 = Card.create(fish: "jellyfish")
+      player1 = Player.create()
+      player2 = Player.create()
+      player1.cards.push(card1)
+      player2.cards.push(card2, card3)
+      player1.ask_for(player2, card1.fish)
+      expect(player2.cards()).to eq([card3])
+    end
   end
-  
+  describe('#ask_for') do
+    it('will ask an opponent if they have a certain card') do
+      card1 = Card.create(fish: "beardfish")
+      card2 = Card.create(fish: "beardfish")
+      card3 = Card.create(fish: "jellyfish")
+      player1 = Player.create()
+      player2 = Player.create()
+      player1.cards.push(card1)
+      player2.cards.push(card2, card3)
+      player1.ask_for(player2, card1.fish)
+      expect(player1.cards()).to eq([card1, card2])
+    end
+  end
+
+
+  # describe "check_doubles" do
+  #   player1 = Player.new()
+  #   player1.get_cards()
+  #   expect(player1.check_doubles()).to eq(true)
+  # end
+  #
 
 end
